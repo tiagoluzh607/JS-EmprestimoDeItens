78aecf9e7c3960697a36748afe7e749f7f99dc97
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-11-30 14:53:55 -0200

    Incluindo Plugin DataTables (Muita Atenção)

--- js/emprestimo-controller.js
+++ js/emprestimo-controller.js
@@ -1,1 +1,20 @@
-console.log('tela de emprestimo');
+var dataSet = [
+    [ "Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ],
+    [ "Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750" ],
+    [ "Ashton Cox", "Junior Technical Author", "San Francisco", "1562", "2009/01/12", "$86,000" ],
+    [ "Unity Butler", "Marketing Designer", "San Francisco", "5384", "2009/12/09", "$85,675" ]
+];
+ 
+$(document).ready(function() {
+    $('#example').DataTable( {
+        data: dataSet,
+        columns: [
+            { title: "Name" },
+            { title: "Position" },
+            { title: "Office" },
+            { title: "Extn." },
+            { title: "Start date" },
+            { title: "Salary" }
+        ]
+    } );
+} );
