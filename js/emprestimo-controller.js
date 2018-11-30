58f25a82b49cd055e9c1d8624d30411bead50495
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-11-30 15:04:54 -0200

    Mudando Tabela Para Atender aos Emprestimos

--- js/emprestimo-controller.js
+++ js/emprestimo-controller.js
@@ -1,20 +1,17 @@
 var dataSet = [
-    [ "Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ],
-    [ "Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750" ],
-    [ "Ashton Cox", "Junior Technical Author", "San Francisco", "1562", "2009/01/12", "$86,000" ],
-    [ "Unity Butler", "Marketing Designer", "San Francisco", "5384", "2009/12/09", "$85,675" ]
+    ["Caderno de Magia", "30/11/2018", "Harry", "25/12/2018"],
+    ["Nimbus 2000", "28/11/2018", "Tony Stark", "10/12/2018"]
 ];
  
 $(document).ready(function() {
     $('#example').DataTable( {
         data: dataSet,
         columns: [
-            { title: "Name" },
-            { title: "Position" },
-            { title: "Office" },
-            { title: "Extn." },
-            { title: "Start date" },
-            { title: "Salary" }
+            { title: "Item" },
+            { title: "Data de Emprestimo" },
+            { title: "Nome" },
+            { title: "Data de Devolucao" }
+
         ]
     } );
 } );
