b95860b07e5d3231a1d999e1e942e14ab444ba3a
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-11-30 10:10:10 -0200

    Logica de Login com Variaveis Fixas e imprimindo no console

--- js/login-controller.js
+++ js/login-controller.js
@@ -1,1 +1,11 @@
-console.log('Aqui eh JS na veia no Controller');
+
+let usuario = 'admin';
+let senha = 'admin';
+
+
+if(usuario == 'admin' && senha == 'admin'){
+    console.log('Logado com sucesso');
+}else{
+    console.log('usuario ou senha invalidos');
+}
+
